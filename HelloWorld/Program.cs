4a5c6a2ac8fb5118fb8157bdb5b186713a8b0f4c
prevostcorentin <corentin.prevost@wildcodeschool.fr>
2019-10-18 18:15:06 +0200

    [+] Full code

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Utilisation: " +
+                                  AppDomain.CurrentDomain.FriendlyName +
+                                  " [nom]");
+                return;
+            }
+            Console.WriteLine("Hello, " + args[0] + " !");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MarvelousHelloWorld
-Hello world that displays your name
+
+This program is associated to a C# quest.
+
+## Compiling
+
+Use Visual Studio on Windows/Mac or Mono on Linux to compile the program.
+
+## Usage
+
+Once you are in the directory of the executable:
+
+```
+$ ./HelloWorld [your name]
+```
+
+## Ownership
+
+Propriety of Wild Code School, written by PREVOST Corentin.
